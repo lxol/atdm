f0da264ad9e725562fd894b1afdf2da38f842e00
Alex Olkhovskiy <alex@olkhovskiy.com>
2016-08-15 06:22:42 +0100

    move Chunker to a separate class

--- src/main/scala/sample/stream/Chunker.scala
+++ src/main/scala/sample/stream/Chunker.scala
@@ -1,5 +1,4 @@
 package sample.stream
-
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{ FileIO, Sink }
@@ -8,26 +7,7 @@ import akka.util.ByteString
 import java.io.File
 import java.io.RandomAccessFile
 import java.nio.ByteOrder
-//import sample.stream.Chunker
 
-object Foo {
-  def main(args: Array[String]): Unit = {
-    import system.dispatcher
-    implicit val system = ActorSystem("atdm")
-    implicit val materializer = ActorMaterializer()
-    val f1 = new File("/tmp/fifo1")
-    //val f1 = new File("/tmp/f1.data")
-    val f2 = new File("/tmp/fifo2")
-    //val f2 = new File("/tmp/f2.data")
-    val source1 = FileIO.fromFile(f1, 9).transform(() => new Chunker(9))
-    val source2 = FileIO.fromFile(f2, 9).transform(() => new Chunker(9))
-    //.runWith(Sink.foreach(println)).onComplete(_ => system.shutdown())
-    source1.merge(source2)
-      .runWith(FileIO.toFile(new File("/tmp/out.data"), false)).onComplete(_ => system.shutdown())
-  }
-}
-
-/*
 class Chunker(val chunkSize: Int) extends PushPullStage[ByteString, ByteString] {
   implicit val order = ByteOrder.LITTLE_ENDIAN
   private var buffer = ByteString.empty
@@ -54,5 +34,3 @@ class Chunker(val chunkSize: Int) extends PushPullStage[ByteString, ByteString] 
     ByteString.newBuilder.putInt(len).append(bytes).result()
   }
 }
- */
-//val chunksStream = rawBytes.transform(() => new Chunker(ChunkLimit))
